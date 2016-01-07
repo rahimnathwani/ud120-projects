7a4a1d38192f95e1a1a600f8ebbe6a1e4afaf738
Rahim Nathwani <rahim@encona.com>
2016-01-07 23:57:41 +0800

    Some more tries.

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -10,7 +10,9 @@ from tester import dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+financial_features = ['salary', 'deferral_payments', 'total_payments', 'loan_advances', 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive', 'restricted_stock', 'director_fees']
+email_features = ['to_messages', 'from_poi_to_this_person', 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
+features_list = ['poi'] + financial_features + email_features
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -32,8 +34,35 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
+# Attempt 0
+# from sklearn.naive_bayes import GaussianNB
+# clf = GaussianNB()
+
+# Attempt 1
+# 	Accuracy: 0.87360	Precision: 0.57602	Recall: 0.19700	F1: 0.29359	F2: 0.22685
+#	Total predictions: 15000	True positives:  394	False positives:  290	False negatives: 1606	True negatives: 12710
+# from sklearn import neighbors
+# clf = neighbors.KNeighborsClassifier(5)
+
+# Attempt 2
+#	Accuracy: 0.86960	Precision: 0.54089	Recall: 0.14550	F1: 0.22931	F2: 0.17041
+#	Total predictions: 15000	True positives:  291	False positives:  247	False negatives: 1709	True negatives: 12753
+from sklearn import neighbors
+clf = neighbors.KNeighborsClassifier(4)
+
+# Attempt 3
+# Precision or recall may be undefined due to a lack of true positive predicitons.
+# from sklearn import neighbors
+# clf = neighbors.KNeighborsClassifier(6)
+
+# Attempt 4
+# Precision or recall may be undefined due to a lack of true positive predicitons.
+# from sklearn import svm
+# clf = svm.SVC()
+
+# Attempt 5
+from sklearn import svm
+clf = svm.SVC(kernel='rbf')
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
