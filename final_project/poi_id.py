1221f9a28145408102bb5f36dfa2eeb1e44de933
Rahim Nathwani <rahim@encona.com>
2016-01-31 15:17:21 +0800

    Precision: 0.36572 Recall: 0.27100

--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -119,7 +119,7 @@ from sklearn import preprocessing
 from sklearn.grid_search import GridSearchCV
 from sklearn.cross_validation import StratifiedShuffleSplit
 scaler = preprocessing.MinMaxScaler()
-parameters = {'pca__n_components': range(3,9), 'svm__kernel':['rbf', ], 'svm__C':[0.1, 1, 10], 'svm__gamma':[10**i for i in range(-3, 4)]}
+parameters = {'pca__n_components': range(3,12), 'svm__kernel':['rbf', ], 'svm__C':[0.01, 0.1, 1, 10, 100], 'svm__gamma':[10**i for i in range(-5, 6)]}
 estimators = [('scaler', scaler), ('pca', PCA(n_components=5)), ('svm', SVC(kernel='rbf'))]
 cv = StratifiedShuffleSplit(
     labels,
